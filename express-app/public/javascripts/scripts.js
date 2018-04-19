59010115014b88d4c92048f2bc4e7fdf8dd20ebf
Usuario <usuariomac@Usuarios-MacBook-Pro.local>
2018-04-19 14:51:59 -0600

    make buttons work with mysql

--- express-app/public/javascripts/scripts.js
+++ express-app/public/javascripts/scripts.js
@@ -1,10 +1,10 @@
 $('document').ready(function() {
     renderExistingSwimlanes();
-   
+
     $('button').on('click', function() {
         var swimlaneName = prompt('New swimlane name');
-        var id = saveSwimlane({name: swimlaneName}); 
-        drawSwimlane(id, swimlaneName);  
+        var id = saveSwimlane({name: swimlaneName});
+        drawSwimlane(id, swimlaneName);
     });
 });
 
@@ -25,7 +25,7 @@ function renderExistingSwimlanes() {
                 var swimlane = swimlanes[i];
                 drawSwimlane(swimlane.id, swimlane.name);
 
-                
+
                 // Get cards for swimlane by swimlaneID
                 renderExistingCards(swimlane.id);
             }
@@ -59,6 +59,54 @@ function getNewId(){
 function drawSwimlane(id, name) {
     newSwimlane = $('<div id="' + id +'" class="swimlane"></div>');
 
+    moveSwimlanes(id,name,newSwimlane);
+
+    var swimlaneHeader = $('<div class="swimlaneHeader">' + name + '</div>');
+    newSwimlane.append(swimlaneHeader);
+
+    var buttons = $('<div class="buttons"><i class="fas fa-trash-alt icons"></i><i class="fas fa-pencil-alt icons"></i><i class="fas fa-plus icons"></i></div>');
+    newSwimlane.append(buttons);
+
+    buttons.on('click', '.fa-trash-alt', function() {
+        var deleteSwimlane = confirm("Are you sure you want to delete swimlane? It may contain cards.");
+            if(deleteSwimlane == false){
+                return null;
+            };
+
+        $(this).closest('.swimlane').remove();
+        removeSwimlane(id);
+
+    });
+
+    buttons.on('click', '.fa-pencil-alt', function() {
+        var newName = prompt('New swimlane name');
+       if (newName == null) {
+          return null;
+        }
+        swimlaneHeader.text(newName);
+        updateSwimlane(id, newName);
+    });
+
+    buttons.on('click', '.fa-plus', function() {
+        var cardHeader = prompt('New card name');
+        if (cardHeader == null) {
+            return null;
+        }
+        var cardDescription = prompt('Card description required');
+        if (cardDescription == null) {
+            return null;
+        }
+
+        var cardId = getNewId();
+        drawCard(cardId, id, cardHeader, cardDescription,newSwimlane);
+        saveCard({ id: cardId, swimlane_id: id, name: cardHeader, cardDescription: cardDescription });
+    })
+
+    $('#swimlanes').append(newSwimlane);
+}
+
+  function moveSwimlanes(id, name, newSwimlane) {
+
     newSwimlane.draggable({
         start: function() {
             $(this).css("zIndex", 100);
@@ -69,76 +117,96 @@ function drawSwimlane(id, name) {
             var otherSwimlane = ui.draggable;
             var thisSwimlane = $(this);
 
-            otherSwimlane.detach();
-            otherSwimlane.insertAfter(thisSwimlane);
-            otherSwimlane.css("zIndex", 0);
+            otherSwimlane.detach().css({ top: 0, left: 0});
+            otherSwimlane.insertBefore(thisSwimlane);
+            otherSwimlane.css("zIndex", 0).appendTo("#swimlanes");
         }
     });
-    var swimlaneHeader = $('<div class="swimlaneHeader">' + name + '</div>');
 
-    newSwimlane.append(swimlaneHeader);
-  
-    var buttons = $('<div class="buttons"><i class="fas fa-trash-alt icons"></i><i class="fas fa-pencil-alt icons"></i><i class="fas fa-plus icons"></i></div>');
+  }
 
-    newSwimlane.append(buttons);
+ function drawCard(swimlaneId, name, cardDescription, cardId, newSwimlane) {
 
-    buttons.on('click', '.fa-trash-alt', function() {
-        $(this).closest('.swimlane').remove();
+  var card = $('<div id="' + cardId +'" class="card"></div>');
 
+        card.draggable({
+        start: function() {
+            $(this).css("zIndex", 100);
+        }
     });
 
-    buttons.on('click', '.fa-pencil-alt', function() {
-      var newName = prompt('New swimlane name');
-      swimlaneHeader.text(newName);  
-      updateSwimlane(id,newName);
-    });
-
-    buttons.on('click', '.fa-plus', function() {
-        var cardHeader = prompt('New card name');
-        var cardDescription = prompt('New card description');
-        var cardId = getNewId();
-        drawCard(id, cardHeader, cardDescription, cardId);
-        //drawCardDescription(id, CardDescription);      
-        saveCard({id: cardId, swimlaneId: id, name: cardHeader, cardDescription: cardDescription});
-        //save description function needed
-    })
-
-    $('#swimlanes').append(newSwimlane);
-}
-
-function drawCard(swimlaneId, name, cardDescription, card_id) {
-    
-        var card = $('<div class="card"></div>');
-
-        card.draggable();
         card.droppable({
-            drop: function(event, ui) {
-                var otherCard = ui.draggable;
-                var thisCard = $(this);
+        drop: function(event, ui) {
+        var otherCard = ui.draggable;
+        var thisCard = $(this);
 
-                otherCard.detach();
-                otherCard.insertBefore(thisCard);
-            }
+        otherCard.detach().css({ top: 0, left: 0 });
+        otherCard.insertAfter(thisCard).appendTo("#" + swimlaneId);
+        otherCard.css("zIndex", 0);
+      }
         });
 
         var cardHeader = $('<div class="cardHeader">' + name + '</div>');
         card.append(cardHeader);
-        var cardButtons = $('<div class="buttons"><i class="fas fa-trash-alt icons"></i><i class="fas cardHeaderEdit fa-pencil-alt icons"></i></div>');
-        card.append(cardButtons);
-        card.append('<div class="cardDescription">' + cardDescription + '</div>')
-        $("#"+ swimlaneId).append(card);
+        var cardButtons = card.append('<div class="buttons"><i class="fas fa-trash-alt icons"></i><i class="fas fa-pencil-alt pencil_card icons"></i></div>');
 
-        cardButtons.on('click', '.fa-trash-alt', function() {
-            $(this).closest('.card').remove();
-        });
-        
-        cardButtons.on('click', '.cardHeaderEdit', function() {
-        var newName = prompt('Card name replacement');
-        cardHeader.text(newName);
-        updateCardName(card_id, newName);
+        cardButtons.on('click', '.fa-trash-alt', function(){
+          var deleteCard = confirm("Are you sure you want to delete swimlane? It may contain cards.");
+              if(deleteCard == false){
+                  return null;
+              };
+              $(this).closest('.card').remove();
+              removeCard(cardId);
+          });
+
+        var cardDescription = $('<div class="cardDescription">' + cardDescription + '</div>');
+    card.append(cardDescription);
+
+    $("#" + swimlaneId).append(card);
+
+    cardButtons.on('click', '.fa-trash-alt', function() {
+        $(this).closest('.card').remove();
     });
-    }
 
+    cardButtons.on('click', '.pencil_card', function() {
+        var newCardName = prompt('New card name');
+        cardHeader.text(newCardName);
+
+        updateCard(cardId, newCardName);
+    });
+
+    cardDescription.on('click', function() {
+        var newCardDescription = prompt("New card description");
+        cardDescription.text(newCardDescription);
+
+        updateCardDescription(cardId, newCardDescription);
+
+    })
+}
+changeTitle();
+//remove swimlane/card from localhost
+
+function removeSwimlane(id){
+   $.ajax({
+           method: "DELETE",
+           url: "http://localhost:8080/swimlanes/" + id
+       })
+       .done(function(swimlane) {
+           alert("Swimlane deleted: " + swimlane);
+       });
+}
+
+function removeCard(id){
+   $.ajax({
+           method: "DELETE",
+           url: "http://localhost:8080/swimlanes/cards/" + id
+           })
+       .done(function(card) {
+           alert("Card deleted: " + card);
+       });
+}
+
+//save from localhost
 function saveSwimlane(swimlane) {
     $.ajax({
             method: "POST",
@@ -146,23 +214,23 @@ function saveSwimlane(swimlane) {
             data: swimlane
         })
         .done(function(swimlane) {
-            alert("Swimlane Saved: " + swimlane.id);
-            return swimlane.id;
+            alert("Swimlane Saved: " + swimlane);
+            //return swimlane.id;
         });
 }
 
-function updateSwimlane(id, name) {
+function updateSwimlane(id, newName) {
   $.ajax({
             method: "POST",
             url: "http://localhost:8080/swimlanes/" + id,
-            data: {name: name}
+            data: {name: newName}
         })
         .done(function(swimlane) {
             alert("Swimlane Update: " + swimlane);
-        });  
+        });
 }
 
-function updateCardName(id, name) {
+function updateCard(id, name) {
   $.ajax({
             method: "POST",
             url: "http://localhost:8080/swimlanes/cards/" + id,
@@ -170,15 +238,26 @@ function updateCardName(id, name) {
         })
         .done(function(card) {
             alert("Card Update: " + card);
-        });  
+        });
+}
+
+function updateCardDescription(id, cardDescription) {
+    $.ajax({
+            method: "POST",
+            url: "http://localhost:8080/swimlanes/cards/" + id,
+            data: { description: cardDescription }
+        })
+        .done(function(card) {
+            alert("Card description Updated: " + card);
+        });
 }
 
 function saveCard(card) {
     $.ajax({
             method: "POST",
             url: "http://localhost:8080/cards",
-            data: card,
-            datatype: 'json'
+            data: card
+            //datatype: 'json'
         })
         .done(function(card) {
             alert("Card Saved: " + card);
